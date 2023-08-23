76987ed136227deede43d2638b62fd518451eb2f
6CROONUX9 <jerchiton.97@gmail.com>
2023-08-23 08:30:02 -0400

    changes were made to the loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,11 +1,11 @@
 const Loader = () => {
     return (
-        <section className="min-h-screen w-screen h-screen bg-white  dark:bg-gray-900   fixed flex  justify-center items-center top-0 left-0 z-50 ">
+        <section className=" min-h-screen w-screen h-screen bg-white  dark:bg-gray-900   fixed flex  justify-center items-center top-0 left-0 z-50 ">
             
-            <div className="absolute top-36 sm:top-12  ">
+            <div className="absolute   ">
                 <img  src="/LogoUser1.png" alt="" />
             </div>
-            <div className=" animate-spin-slow">
+            <div className="absolute animate-spin-slow">
                 <img   src="/LogoUser2.png" alt="" />
             </div>
 
